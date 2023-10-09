2d46c844ce30e7fb545649b2e9607836ef5efc9d
Monique Rodriguez <moniquerdz@gmail.com>
2023-10-09 13:49:46 -0500

    using composition to pass card css styling to expenses

--- appOne/src/components/Card.js
+++ appOne/src/components/Card.js
@@ -1,8 +1,10 @@
 import './Card.css'
 
 
-function Card(){
-    return <div className="card"></div>
+function Card(props){
+    //this is used so that anything received as a class name from outside is added to that string.
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>
 
 }
 
