df0e0ad9274d7679feb00982084dee1b557c6221
Monique Rodriguez <moniquerdz@gmail.com>
2023-10-09 14:55:33 -0500

    changed fucntions to arrow functions

--- appOne/src/components/UI/Card.js
+++ appOne/src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import './Card.css'
 
 
-function Card(props){
+const Card =(props) =>{
     //this is used so that anything received as a class name from outside is added to that string.
     const classes = 'card ' + props.className;
     return <div className={classes}>{props.children}</div>
